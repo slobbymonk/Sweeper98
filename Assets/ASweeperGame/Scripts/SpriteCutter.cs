361c4b3b38574209f8310e281b992fefbe40ffd1
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2026-03-31 17:19:01 +0200

    Player Controller + Cutout now also visual

---
+++ Assets/ASweeperGame/Scripts/SpriteCutter.cs
@@ -0,0 +1,19 @@
+﻿using Clipper2Lib;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteCutter : MonoBehaviour
+{
+    [SerializeField] private SpriteMask _mask;
+
+    private void Awake()
+    {
+        _mask.enabled = false;
+    }
+
+    [Button]
+    public void Cut()
+    {
+        _mask.enabled = true;
+    }
+}
