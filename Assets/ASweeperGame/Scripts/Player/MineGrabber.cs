8cc8f038fcce472517a1ac40debc4da08e7e0082
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2026-03-31 19:10:09 +0200

    Victor don't cause my things to reset again!

--- Assets/ASweeperGame/Scripts/Player/MineGrabber.cs
+++ Assets/ASweeperGame/Scripts/Player/MineGrabber.cs
@@ -4,12 +4,22 @@ public class MineGrabber : MonoBehaviour
 {
     private Mine _currentlyHeldMine;
 
+    [SerializeField] private Sprite _bombHoldingSprite;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    private Sprite _defaultSprite;
+
+    private void Awake()
+    {
+        _defaultSprite = _spriteRenderer.sprite;
+    }
+
     void LaunchBomb()
     {
         if (_currentlyHeldMine == null) return;
 
         // Launch logic
 
+        _spriteRenderer.sprite = _defaultSprite;
         _currentlyHeldMine = null;
     }
 
@@ -18,5 +28,15 @@ public class MineGrabber : MonoBehaviour
         if(mine != null) return false;
         _currentlyHeldMine = mine;
         return true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.TryGetComponent<Mine>(out var mine))
+        {
+            if (!TryHoldMine(mine)) return;
+            mine.transform.parent = transform;
+            _spriteRenderer.sprite = _bombHoldingSprite;
+        }
     }
 }
