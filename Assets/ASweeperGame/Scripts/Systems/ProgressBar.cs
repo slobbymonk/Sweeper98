52145003c9d48bdc90d1c115ce2342e4e85f19c8
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2026-04-01 12:20:57 +0200

    Progress bar functional & prefab

---
+++ Assets/ASweeperGame/Scripts/Systems/ProgressBar.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour, IDraggable
+{
+    public bool HasBeenDragged { get; set; }
+
+    private float _timePlayed;
+
+    [SerializeField] private Image _fillImage;
+    [SerializeField] private TMP_Text _progressText;
+    [SerializeField] private float _timeBeforeFull;
+
+
+    void Update()
+    {
+        _timePlayed += Time.deltaTime;
+
+        float progressPercentage = (_timePlayed / _timeBeforeFull) * 100;
+        string progressPercentageString = progressPercentage.ToString("F0");
+        _progressText.text = $"{progressPercentageString}%";
+        _fillImage.fillAmount = progressPercentage / 100;
+    }
+}
