46a8e1e89e6356815aebbf1c665ce94e1154988b
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2026-04-01 17:21:32 +0200

    Push test

--- Assets/ASweeperGame/Scripts/Player/PlayerHealth.cs
+++ Assets/ASweeperGame/Scripts/Player/PlayerHealth.cs
@@ -1,4 +1,5 @@
 ﻿using FMODUnity;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,9 +7,22 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private EventReference _deathSound;
 
+    [SerializeField] private List<Collision2D> _collidersBeingTouched;
+
     public void Die()
     {
         RuntimeManager.PlayOneShot(_deathSound);
         GameStateManager.Instance.ChangeState(GameStateManager.GameState.GameOver);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!_collidersBeingTouched.Contains(collision)) return;
+
+        
     }
 }
