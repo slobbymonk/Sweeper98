1d6bf0c26bedae3c8a784639bf4143c29a0cd69b
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2026-04-01 10:23:56 +0200

    Made it so clippy can talk

---
+++ Assets/ASweeperGame/Scripts/Player/PlayerTalker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class PlayerTalker : MonoBehaviour
+{
+    [SerializeField] private GameObject _bubble;
+    [SerializeField] private TMP_Text _text;
+
+    [Button]
+    public void DebugShowText()
+    {
+        ShowText("It looks like I've got a bomb, hold space.", 5);
+    }
+    public void ShowText(string text, float duration)
+    {
+        StartCoroutine(ShowTextRoutine(text, duration));
+    }
+
+    public IEnumerator ShowTextRoutine(string text, float duration)
+    {
+        _bubble.SetActive(true);
+        _text.text = text;
+        yield return new WaitForSeconds(duration);
+        _bubble.SetActive(false);
+    }
+}
